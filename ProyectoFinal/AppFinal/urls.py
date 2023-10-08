e110f5a87c57d74ac1310c6da1d786b1baf1ea2c
VERO MENICHETTI <ingvmenichetti@gmail.com>
2023-10-07 22:35:14 -0300

    Se creo el CRUD de Peliculas

--- ProyectoFinal/AppFinal/urls.py
+++ ProyectoFinal/AppFinal/urls.py
@@ -8,12 +8,14 @@ urlpatterns = [
     path('peliculas/',peliculas, name="peliculas"),
     path('series/',series, name="series"),
     path('usuarios/',usuarios, name="usuarios"),
-    path('formulario-pelicula/',formPelicula,name="formularioPelicula"), 
+    path('formulario-pelicula/',crear_pelicula,name="formularioPelicula"), 
     path('formulario-serie/',formSerie,name="formularioSerie"), 
     path('lista-peliculas/', lista_peliculas, name='ListaPeliculas'),
     path('busqueda-pelicula/',busquedaPelicula,name="BusquedaPelicula"),
     path('buscar/',buscar,name="Buscar"),
     path('error/',error,name="error404"),
+    path('eliminarPeliculas/<int:id>', eliminar_pelicula, name="EliminarPeliculas"),
+    path('editarPelicula/<int:id>', editar_pelicula, name="EditarPelicula"),
 ]
 
 if settings.DEBUG:
--- ProyectoFinal/AppFinal/views.py
+++ ProyectoFinal/AppFinal/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest,HttpResponse
 from .models import *
 from .forms import FormPelicula,FormSerie
-from flask import Flask, request
+from django import forms
+
 
 
 
@@ -40,16 +41,38 @@ def error(req):
     return render(req, "error404.html")
 
 
-# FORMULARIOS
+# CRUD PELICULAS
 
-def formPelicula(req):
-    print('method',req.method)
-    print('POST',req.POST)
+# Funcion Registro/Creacion
 
-    if req.method == 'POST':
-        miFormulario=FormPelicula(req.POST)
-        if miFormulario.is_valid():
-            data=miFormulario.cleaned_data
+# def formPelicula(req):
+    # print('method',req.method)
+    # print('POST',req.POST)
+
+    # if req.method == 'POST':
+    #     miFormulario=FormPelicula(req.POST)
+    #     if miFormulario.is_valid():
+    #         data=miFormulario.cleaned_data
+    #         pelicula=Pelicula(
+    #             nombre=data['nombre'],
+    #             subtitulo=data['subtitulo'],
+    #             imagenpelicula=data['imagen'],
+    #             descripcion=data['descripcion'],
+    #             reseña=data['reseña'],
+    #             youtube=data['youtube'])
+    #         pelicula.save()
+
+    #     return render(req,"formPelicula.html")
+    
+    # else:
+    #     miFormulario=FormPelicula()
+    #     return render(req, "formPelicula.html",{"miFormulario":miFormulario})
+
+def crear_pelicula(request):
+    if request.method == 'POST':
+        form = FormPelicula(request.POST, request.FILES)
+        if form.is_valid():
+            data=form.cleaned_data
             pelicula=Pelicula(
                 nombre=data['nombre'],
                 subtitulo=data['subtitulo'],
@@ -58,12 +81,60 @@ def formPelicula(req):
                 reseña=data['reseña'],
                 youtube=data['youtube'])
             pelicula.save()
-
-        return render(req,"formPelicula.html")
-    
+            return redirect('ListaPeliculas')  # Cambia 'lista_peliculas' por la URL de tu vista de lista de películas
     else:
-        miFormulario=FormPelicula()
-        return render(req, "formPelicula.html",{"miFormulario":miFormulario})
+        form = PeliculaForm()
+    return render(request, 'formPelicula.html', {'form': form})
+
+# Funcion Eliminar Pelicula
+
+def eliminar_pelicula(req, id):
+
+    if req.method == 'POST':
+
+        pelicula = Pelicula.objects.get(id=id)
+        pelicula.delete()
+
+        pelicula = Pelicula.objects.all()
+
+        return render(req, "lista_peliculas.html", {"peliculas": peliculas})
+
+# Funcion Editar Pelicula
+    
+# Función Editar
+    
+def editar_pelicula(req, id):
+        
+    pelicula = Pelicula.objects.get(id=id)
+
+    if req.method == 'POST':
+
+        miFormulario = FormPelicula(req.POST)
+
+        if miFormulario.is_valid():
+
+            data = miFormulario.cleaned_data
+            pelicula.nombre=data['nombre'],
+            pelicula.subtitulo=data['subtitulo'],
+            pelicula.imagenpelicula=data['imagen'],
+            pelicula.descripcion=data['descripcion'],
+            pelicula.reseña=data['reseña'],
+            pelicula.youtube=data['youtube']
+            pelicula.save()
+
+            return render(req, "inicio.html")
+    else:
+
+        miFormulario = FormPelicula(initial={
+            "nombre": pelicula.nombre,
+            "subtitulo": pelicula.subtitulo,
+            "imagen": pelicula.imagenpelicula,
+            "descripcion": pelicula.descripcion,
+            "reseña": pelicula.reseña,
+            "youtube": pelicula.youtube,
+        })
+        return render(req, "editarPelicula.html", {"miFormulario": miFormulario, "id": pelicula.id})
+
 
 def formSerie(req):
     print('method',req.method)
@@ -103,3 +174,10 @@ def buscar(req:HttpRequest):
     
     else:
         return HttpResponse(f'Debe agregar una pelicula')
+    
+class PeliculaForm(forms.ModelForm):
+    class Meta:
+        model = Pelicula
+        fields = ['nombre', 'subtitulo', 'imagenpelicula', 'descripcion', 'reseña', 'youtube']
+
+
